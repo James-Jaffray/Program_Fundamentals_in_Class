3121cadc33c6b9c1d287195f1ca0f2127df7d4c7
James Jaffray <james.v.jaffray@gmail.com>
2026-09-18 11:01:23 -0600

    day 7 overview

--- Day 06/exercises/heads_or_tails.py
+++ Day 06/exercises/heads_or_tails.py
@@ -7,11 +7,14 @@ if userguess == "heads":
 elif userguess == "tails":
     userguess = 0
 else:
-    print("Please input heads or tails:")
+    print("Please input heads or tails- run again")
     exit()
 
 coinflip = random.randint(0, 1)
-
+if coinflip == 0:
+    print("PC flipped: Tails")
+elif coinflip == 1:
+    print("PC flipped: Heads")
 
 if userguess == coinflip:
     print("Correct!")
--- Day 06/exercises/leap_year.py
+++ Day 06/exercises/leap_year.py
@@ -1,8 +1,7 @@
 year = int(input("Please enter a year: "))
 
-if (year % 4) == 0 and (year % 100) != 0 and (year % 400) != 0:
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(f"{year} is a leap? True")
-
 else:
     print(f"{year} is a leap year? False")
 
